a5fef7bd15297341088edb781addf6725cfe892e
Gagan Suman <fbdgagansuman@gmail.com>
2024-12-24 18:12:36 +0530

    some minor but essential changes

--- backend/utils/dbconnections.js
+++ backend/utils/dbconnections.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 function connectToDb(){
-    return mongoose.connect(`mongodb+srv://vignvick3005:${process.env.DB_PASSWORD}@clustersharpener.ru5nn.mongodb.net/makeholydaydev?retryWrites=true&w=majority&appName=ClusterSharpener`)
+    return mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@clustersharpener.ru5nn.mongodb.net/${process.env.DB_PROJECT_NAME}?retryWrites=true&w=majority&appName=ClusterSharpener`)
 }
 
 module.exports = connectToDb;
